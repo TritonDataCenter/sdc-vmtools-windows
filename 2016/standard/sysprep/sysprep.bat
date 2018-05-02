2ea4f71d8a9986c5d81c7365644e8260704987c6
Derek Crudgington <derek@joyent.com>
2018-05-02 14:01:55 -0500

    Update sysprep.bat

--- 2016/standard/sysprep/sysprep.bat
+++ 2016/standard/sysprep/sysprep.bat
@@ -2,8 +2,6 @@
 
 REM The sysprep batch file will run sysprep against the unattend.xml
 REM file, which has all the instructions required to turn the system into
-REM a "gold master" image. Note that the unattend.xml will normally contain
-REM the PRODUCTKEY value, which is the product key for the version of Windows 
-REM you are running.
+REM a "gold master" image.
 
 c:\windows\system32\sysprep\sysprep /generalize /oobe /shutdown /unattend:c:\smartdc\sysprep\windows2008r2.xml
