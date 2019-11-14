75af35407e83dcb54bb6dc72dfbdb0cfcec24edc
Joel Garboden <joel.garboden@logos.com>
2019-11-13 16:57:50 -0800

    Update CustomSetupSteps practical use

--- kvm/2019/smartdc/lib/CustomSetupSteps.ps1
+++ kvm/2019/smartdc/lib/CustomSetupSteps.ps1
@@ -1,4 +1,21 @@
-Write-Host "Custom steps"
-
 $vmalias = & "C:\smartdc\bin\mdata-get.exe" "sdc:alias"
 $administrator_pw = & "C:\smartdc\bin\mdata-get.exe" "administrator_pw"
+
+if (
+  [string]::IsNullOrEmpty($vmalias) -or
+  $vmalias.StartsWith("No metadata for") -or
+  [string]::IsNullOrEmpty($administrator_pw) -or 
+  $administrator_pw.StartsWith("No metadata for")
+) {
+  Write-Host "Failed to retrieve metadata"
+  Start-Sleep -s 15
+
+  $vmalias = & "C:\smartdc\bin\mdata-get.exe" "sdc:alias"
+  $administrator_pw = & "C:\smartdc\bin\mdata-get.exe" "administrator_pw"
+}
+
+$securePassword = ConvertTo-SecureString -AsPlainText -Force -String $administrator_pw
+$adminAccount = Get-LocalUser -Name "Administrator"
+$adminAccount | Set-LocalUser -Password $securePassword
+
+Rename-Computer -NewName $vmalias -restart
