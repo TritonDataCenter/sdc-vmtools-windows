9a7acc575cc8bd8a6e15a60946cd71957bef1b84
Joel Garboden <joel.garboden@logos.com>
2019-11-18 08:51:01 -0800

    Remove redundant batch password logic

--- kvm/2019/smartdc/lib/SetupComplete.cmd
+++ kvm/2019/smartdc/lib/SetupComplete.cmd
@@ -1,7 +1,5 @@
 @echo off
 >c:\smartdc\setup_output.txt (
-  for /f "delims=" %%a in ('C:\smartdc\bin\mdata-get.exe administrator_pw') do @set password=%%a
-  net user administrator "%password%"
   diskpart /s C:\smartdc\lib\diskpart_commands
   PowerShell.exe -ExecutionPolicy UnRestricted -File C:\smartdc\lib\CustomSetupSteps.ps1
 )
